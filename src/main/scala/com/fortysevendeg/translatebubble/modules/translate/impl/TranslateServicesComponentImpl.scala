efebd50fb3f1ecad06177ac37b0c6a8c05d11835
Javi Pacheco <javielinux@gmail.com>
2014-12-18 23:14:31 +0100

    Try monad implementation

--- src/main/scala/com/fortysevendeg/translatebubble/modules/translate/impl/TranslateServicesComponentImpl.scala
+++ src/main/scala/com/fortysevendeg/translatebubble/modules/translate/impl/TranslateServicesComponentImpl.scala
@@ -16,6 +16,7 @@ import org.json4s.native.JsonMethods._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+import scala.util.{Failure, Success, Try}
 
 trait TranslateServicesComponentImpl
     extends TranslateServicesComponent {
@@ -30,59 +31,37 @@ trait TranslateServicesComponentImpl
         Future {
           request.text.map {
             text =>
-              var urlMaybe: Option[String] = None
-              try {
-                urlMaybe = Some("http://api.mymemory.translated.net/get?q="
-                    + URLEncoder.encode(text, "UTF-8")
-                    + "&langpair=" + URLEncoder.encode(TypeLanguageTransformer.toMyMemory(request.from)
-                    + "|" + TypeLanguageTransformer.toMyMemory(request.to), "UTF-8"))
-              } catch {
-                case e: UnsupportedEncodingException => e.printStackTrace()
-              }
-
-              urlMaybe.map(
-                f = url => {
-                  try {
-                    val httpClient: DefaultHttpClient = new DefaultHttpClient
-                    val httpPost: HttpGet = new HttpGet(url)
-                    val httpResponse: HttpResponse = httpClient.execute(httpPost)
-                    val httpEntity: HttpEntity = httpResponse.getEntity
-                    val is: InputStream = httpEntity.getContent
-                    val reader: BufferedReader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8)
-                    val sb: StringBuilder = new StringBuilder
-                    var line: String = null
-                    while ( {
-                      line = reader.readLine
-                      line
-                    } != null) {
-                      sb.append(line + "\n")
-                    }
-                    is.close()
-
-                    implicit val formats = org.json4s.DefaultFormats
-                    val json = parse(sb.toString())
-                    val translatedText = (json \ "responseData" \ "translatedText").extract[String]
-                    TranslateResponse(Some(translatedText))
-                  } catch {
-                    case e: UnsupportedEncodingException => {
-                      e.printStackTrace()
-                      TranslateResponse(None)
-                    }
-                    case e: ClientProtocolException => {
-                      e.printStackTrace()
-                      TranslateResponse(None)
-                    }
-                    case e: IOException => {
-                      e.printStackTrace()
-                      TranslateResponse(None)
-                    }
-                    case e: Throwable => {
-                      e.printStackTrace()
-                      TranslateResponse(None)
-                    }
-                  }
+              Try {
+                val url = "http://api.mymemory.translated.net/get?q=" + URLEncoder.encode(text, "UTF-8") +
+                    "&langpair=" + URLEncoder.encode(TypeLanguageTransformer.toMyMemory(request.from) +
+                    "|" + TypeLanguageTransformer.toMyMemory(request.to), "UTF-8")
+                val httpClient: DefaultHttpClient = new DefaultHttpClient
+                val httpPost: HttpGet = new HttpGet(url)
+                val httpResponse: HttpResponse = httpClient.execute(httpPost)
+                val httpEntity: HttpEntity = httpResponse.getEntity
+                val is: InputStream = httpEntity.getContent
+                val reader: BufferedReader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"), 8)
+                val sb: StringBuilder = new StringBuilder
+                var line: String = null
+                while ( {
+                  line = reader.readLine
+                  line
+                } != null) {
+                  sb.append(line + "\n")
                 }
-              ).getOrElse(TranslateResponse(None))
+                is.close()
+
+                implicit val formats = org.json4s.DefaultFormats
+                val json = parse(sb.toString())
+                val translatedText = (json \ "responseData" \ "translatedText").extract[String]
+                TranslateResponse(Some(translatedText))
+              } match {
+                case Success(response) => response
+                case Failure(ex) => {
+                  ex.printStackTrace()
+                  TranslateResponse(None)
+                }
+              }
           }.getOrElse(TranslateResponse(None))
         }
     }
