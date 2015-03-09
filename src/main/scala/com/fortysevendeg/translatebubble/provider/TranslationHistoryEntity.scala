1f29ab4dd9150faeb6939147e89956a3dc17af31
franciscodr <francisco.d@47deg.com>
2015-03-09 18:37:41 +0100

    TB-8: Removes some comments in TranslationHistoryEntity case class

--- src/main/scala/com/fortysevendeg/translatebubble/provider/TranslationHistoryEntity.scala
+++ src/main/scala/com/fortysevendeg/translatebubble/provider/TranslationHistoryEntity.scala
@@ -3,63 +3,7 @@ package com.fortysevendeg.translatebubble.provider
 import android.database.Cursor
 import com.fortysevendeg.translatebubble.utils.LanguageType.LanguageType
 
-case class TranslationHistoryEntity(id: Int, data: TranslationHistoryEntityData) {
-
-  //  def get(contentResolver: ContentResolver, id: Int): Option[TranslationHistoryEntityData] = {
-  //    val cursor = contentResolver.query(
-  //      TranslateBubbleContentProvider.contentUriTranslationHistory,
-  //      TranslationHistoryEntity.allFields.toArray,
-  //      TranslateBubbleSqlHelper.id + " = ?",
-  //      Seq(id.toString).toArray,
-  //      "")
-  //
-  //    get(Option[Cursor](cursor))
-  //  }
-  //
-  //  def list(contentResolver: ContentResolver, where: String, sort: String): List[TranslationHistoryEntityData] = {
-  //    val cursor = contentResolver.query(
-  //      TranslateBubbleContentProvider.contentUriTranslationHistory,
-  //      TranslationHistoryEntity.allFields.toArray,
-  //      where,
-  //      null,
-  //      sort)
-  //
-  //    list(Option[Cursor](cursor))
-  //  }
-  //
-  //  private def get(cursor: Option[Cursor]): Option[TranslationHistoryEntityData] = {
-  //    cursor match {
-  //      case Some(cursorItem) if cursorItem.moveToFirst() => {
-  //        val single = translationHistoryEntityDataFromCursor(cursorItem)
-  //        cursorItem.close()
-  //        Some(single)
-  //      }
-  //      case _ => None
-  //    }
-  //  }
-  //
-  //  private def list(cursor: Option[Cursor]): List[TranslationHistoryEntityData] = {
-  //
-  //    def listLoop(cursor: Cursor, result: List[TranslationHistoryEntityData]): List[TranslationHistoryEntityData] = {
-  //      cursor match {
-  //        case validCursor if validCursor.moveToNext() => listLoop(
-  //          cursor,
-  //          translationHistoryEntityDataFromCursor(validCursor) :: result)
-  //        case _ => result.reverse
-  //      }
-  //    }
-  //
-  //    cursor match {
-  //      case Some(cursorItem) if cursorItem.moveToFirst() => {
-  //        val result = listLoop(cursorItem, List.empty)
-  //        cursorItem.close()
-  //        result
-  //      }
-  //      case _ => List.empty
-  //    }
-  //  }
-
-}
+case class TranslationHistoryEntity(id: Int, data: TranslationHistoryEntityData)
 
 case class TranslationHistoryEntityData(
     originalText: String,
