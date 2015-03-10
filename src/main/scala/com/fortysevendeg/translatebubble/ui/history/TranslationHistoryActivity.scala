c151c020386ad40db9e00919e09d8a95fd8a3b70
franciscodr <francisco.d@47deg.com>
2015-03-10 16:46:49 +0100

    TB-8: Enables home button in TranslationHistoryActivity

--- src/main/scala/com/fortysevendeg/translatebubble/ui/history/TranslationHistoryActivity.scala
+++ src/main/scala/com/fortysevendeg/translatebubble/ui/history/TranslationHistoryActivity.scala
@@ -2,12 +2,14 @@ package com.fortysevendeg.translatebubble.ui.history
 
 import android.app.Activity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.{GridLayoutManager, LinearLayoutManager}
+import android.view.MenuItem
+import com.fortysevendeg.macroid.extras.DeviceMediaQueries._
 import com.fortysevendeg.macroid.extras.RecyclerViewTweaks._
 import com.fortysevendeg.translatebubble.modules.ComponentRegistryImpl
 import com.fortysevendeg.translatebubble.modules.repository.FetchAllTranslationHistoryRequest
 import com.fortysevendeg.translatebubble.provider.TranslationHistoryEntity
-import com.fortysevendeg.translatebubble.ui.commons.{LineItemDecorator, ListLayout}
+import com.fortysevendeg.translatebubble.ui.commons.{HistoryItemDecorator, ListLayout}
 import macroid.FullDsl._
 import macroid.{AppContext, Contexts, Ui}
 
@@ -25,16 +27,34 @@ class TranslationHistoryActivity
     super.onCreate(savedInstanceState)
     setContentView(content)
 
+    getActionBar.setDisplayHomeAsUpEnabled(true)
+    getActionBar.setHomeButtonEnabled(true)
+
+    val layoutManager = if (landscapeTablet) {
+      new GridLayoutManager(appContextProvider.get, 3)
+    } else if (tablet) {
+      new GridLayoutManager(appContextProvider.get, 2)
+    } else {
+      new LinearLayoutManager(appContextProvider.get)
+    }
+
     runUi(
       (recyclerView
-          <~ rvLayoutManager(new LinearLayoutManager(appContextProvider.get))
-          <~ rvAddItemDecoration(new LineItemDecorator)) ~
+          <~ rvLayoutManager(layoutManager)
+          <~ rvAddItemDecoration(new HistoryItemDecorator)) ~
           (reloadButton <~ On.click(Ui {
             loadTranslationHistory()
           })))
 
     loadTranslationHistory()
 
+  }
+
+  override def onOptionsItemSelected(item: MenuItem): Boolean = {
+    item.getItemId match {
+      case android.R.id.home => finish(); true
+      case _ => super.onOptionsItemSelected(item)
+    }
   }
 
   def loadTranslationHistory(): Unit = {
